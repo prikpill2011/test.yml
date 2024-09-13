62f00c624a68ed2cf50173eb1fed7151d8c112a8
prikpill2011 <prikpill2011@gmail.com>
2024-09-13 02:46:20 -0400

    code update

--- cypress/e2e/login spec.cy.js
+++ cypress/e2e/login spec.cy.js
@@ -1,12 +1,12 @@
- //Test case1- Valid loginStandard User Login 
- describe('Standard User Login', () => {
-    const username = 'standard_user';
-    const password = 'secret_sauce';
+ //Test case1- Valid User Login 
+ describe(' Login', () => {
+    const username = 'fhrbmj';
+    const password = 'password';
     
     it('successfully logs in with valid credentials', () => {
-      cy.visit('www.saucedemo.com');
-      cy.get('#user-name').type(username);
-      cy.get('#password').type(password);
+      cy.visit('https://thinking-tester-contact-list.herokuapp.com/');
+      cy.get('username').type("fhrbmj");
+      cy.get('#password').type("password");
       cy.get('#login-button').click();
      // cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user in this service');  
     cy.timeout (['time-out'], 10000)
