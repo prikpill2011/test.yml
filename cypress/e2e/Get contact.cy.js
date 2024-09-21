848bb2198e4851e6f2173f8e8885804c9facea8b
prikpill2011 <prikpill2011@gmail.com>
2024-09-20 21:28:03 -0400

    yes

--- cypress/e2e/Get contact.cy.js
+++ cypress/e2e/Get contact.cy.js
@@ -3,7 +3,7 @@
 
 describe('Get Contacts', () => {
 it('returns a list of contacts', () => {
-cy.request('GET', '/contacts')
+cy.request('GET', 'https://thinking-tester-contact-list.herokuapp.com/')
 .should((response) => {
 expect(response.status).to.eq(200)
 expect(response.body).to.be.an('array')
@@ -15,7 +15,7 @@ expect(response.body).to.be.an('array')
 describe('Create Contact', () => {
 it('creates a new contact', () => {
 const contact = { name: 'John Doe', email: 'johndoe@example.com' }
-cy.request('POST', '/contacts', contact)
+cy.request('POST', 'https://thinking-tester-contact-list.herokuapp.com/', contact)
 .should((response) => {
 expect(response.status).to.eq(201)
 expect(response.body).to.have.property('id')
